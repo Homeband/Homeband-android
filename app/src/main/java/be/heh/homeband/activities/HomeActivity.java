caf2fb85dc4532d2962ed772608e4f77f7dc846a
christopher <33717563+Saovers@users.noreply.github.com>
2018-01-26 13:02:58 +0100

    Add home view

---
+++ app/src/main/java/be/heh/homeband/activities/HomeActivity.java
@@ -0,0 +1,51 @@
+package be.heh.homeband.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import be.heh.homeband.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_place:
+
+                    return true;
+                case R.id.navigation_favorite:
+
+                    return true;
+                case R.id.navigation_settings:
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    
+
+}
