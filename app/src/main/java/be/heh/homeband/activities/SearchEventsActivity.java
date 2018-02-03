cd77dea13dfa6efbf621c07eacb73f66db439336
christopher <33717563+Saovers@users.noreply.github.com>
2018-02-03 15:29:14 +0100

    View groupe + début events

---
+++ app/src/main/java/be/heh/homeband/activities/SearchEventsActivity.java
@@ -0,0 +1,66 @@
+package be.heh.homeband.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import be.heh.homeband.R;
+
+/**
+ * Created by christopher on 03/02/2018.
+ */
+
+public class SearchEventsActivity extends AppCompatActivity {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+
+                    Intent intent = new Intent (getApplicationContext(),HomeActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.navigation_place:
+
+
+                    Intent intent2 = new Intent (getApplicationContext(),SearchGroupeActivity.class);
+                    startActivity(intent2);
+                    break;
+                case R.id.navigation_favorite:
+
+                    return true;
+                case R.id.navigation_settings:
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchevents);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+}
