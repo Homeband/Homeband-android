3714834de46fdc442244fd325a643812d24c232c
christopher <33717563+Saovers@users.noreply.github.com>
2018-02-12 21:02:55 +0100

    Event spinner fill + supression array

--- app/src/main/java/be/heh/homeband/activities/SearchEventsActivity.java
+++ app/src/main/java/be/heh/homeband/activities/SearchEventsActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -16,15 +18,32 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import org.w3c.dom.Text;
 
+import java.lang.reflect.Type;
+import java.util.List;
+
 import be.heh.homeband.R;
+import be.heh.homeband.app.HomebandApiInterface;
+import be.heh.homeband.app.HomebandApiReponse;
+import be.heh.homeband.app.HomebandRetrofit;
+import be.heh.homeband.entities.Style;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by christopher on 03/02/2018.
  */
 
 public class SearchEventsActivity extends AppCompatActivity {
+    ArrayAdapter<Style> adapterStyle;
+    Spinner spinStyle;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -57,6 +76,9 @@ public class SearchEventsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchevents);
+
+        initialisation();
+        initStyles();
 
         Button next = (Button) findViewById(R.id.btnGroupe);
         next.setOnClickListener(new View.OnClickListener() {
@@ -97,15 +119,81 @@ public class SearchEventsActivity extends AppCompatActivity {
 
             }
         });
-        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
+    }
+    public void initialisation(){
+        this.spinStyle = (Spinner)findViewById(R.id.spinner1);
     }
 
+    public void initStyles(){
+        try {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(HomebandRetrofit.API_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            // Création d'une instance du service avec Retrofit
+            HomebandApiInterface serviceApi = retrofit.create(HomebandApiInterface.class);
+
+            // Requête vers l'API
+            serviceApi.getStyles().enqueue(new Callback<HomebandApiReponse>() {
+                @Override
+                public void onResponse(Call<HomebandApiReponse> call, Response<HomebandApiReponse> response) {
+
+                    // En fonction du code HTTP de Retour (2** = Successful)
+                    if (response.isSuccessful()) {
+
+                        // Récupération de la réponse de l'API
+                        HomebandApiReponse res = response.body();
+                        res.mapResultat();
+
+                        CharSequence messageToast;
+                        if (res.isOperationReussie() == true) {
+                            // Element de retour sera de type List<style>
+                            Type typeListe = new TypeToken<List<Style>>(){}.getType();
+
+                            // Désérialisation du tableau JSON (JsonArray) en liste d'objets Style
+
+                            //gson.fromJson prend 2 paramètres
+                            //Premier paramètre c'est l'élément Json qu'il va falloir récupérer
+                            //Deuxième paramètre c'est le type d'élément à récupérer
+                            Gson gson = new Gson();
+                            List<Style> listeStyles = gson.fromJson(res.get("styles").getAsJsonArray(), typeListe);
+
+                            // Initialisation de l'adapter
+                            adapterStyle = new ArrayAdapter<Style>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item);
+
+                            // Ajout de la liste des styles à l'adapter
+                            adapterStyle.addAll(listeStyles);
+
+                            // Application de l'adapter au spinner
+                            spinStyle.setAdapter(adapterStyle);
+                        } else {
+                            messageToast = "Échec de la connexion\r\n" + res.getMessage();
+
+                            // Affichage d'un toast pour indiquer le résultat
+                            Toast toast = Toast.makeText(getApplicationContext(), messageToast, Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                        }
+                    } else {
+                        int statusCode = response.code();
+
+                        String res = response.toString();
+                        CharSequence message ="Erreur lors de l'appel à l'API (" + statusCode +")";
+                        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<HomebandApiReponse> call, Throwable t) {
+                    Log.d("LoginActivity", t.getMessage());
+                }
+            });
+        } catch (Exception e){
+            Toast.makeText(this,(CharSequence)"Exception",Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
 }
